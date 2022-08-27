6280c1a074e3c6af7a84d1ef37f75985bdda858e
baba-s <shotababa1206@gmail.com>
2022-08-27 18:18:32 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.SpriteRendererExtensionMethods
+# Kogane Sprite Renderer Extension Methods
+
+SpriteRenderer の拡張メソッド
---
+++ Runtime/SpriteRendererExtensionMethods.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Kogane
+{
+    public static class SpriteRendererExtensionMethods
+    {
+        public static void SetAlpha( this SpriteRenderer self, float alpha )
+        {
+            var color = self.color;
+            color.a    = alpha;
+            self.color = color;
+        }
+    }
+}
