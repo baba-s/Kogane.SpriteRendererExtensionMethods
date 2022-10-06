89b7404e5bfaf6b31c682ce86ac9897cd3ec8de7
baba-s <shotababa1206@gmail.com>
2022-10-06 21:04:46 +0900

    update

--- Runtime/SpriteRendererExtensionMethods.cs
+++ Runtime/SpriteRendererExtensionMethods.cs
@@ -10,5 +10,49 @@ namespace Kogane
             color.a    = alpha;
             self.color = color;
         }
+
+        public static Vector2 GetSize( this SpriteRenderer self )
+        {
+            return new Vector2( self.GetWidth(), self.GetHeight() );
+        }
+
+        public static float GetWidth( this SpriteRenderer self )
+        {
+            return self.bounds.size.x;
+        }
+
+        public static float GetHeight( this SpriteRenderer self )
+        {
+            return self.bounds.size.y;
+        }
+
+        public static Rect GetWorldEdge( this SpriteRenderer self )
+        {
+            return new
+            (
+                position: self.transform.position,
+                size: self.bounds.size
+            );
+        }
+
+        public static float GetWorldLeft( this SpriteRenderer self )
+        {
+            return self.transform.position.x - self.bounds.size.x / 2;
+        }
+
+        public static float GetWorldRight( this SpriteRenderer self )
+        {
+            return self.transform.position.x + self.bounds.size.x / 2;
+        }
+
+        public static float GetWorldBottom( this SpriteRenderer self )
+        {
+            return self.transform.position.y - self.bounds.size.y / 2;
+        }
+
+        public static float GetWorldTop( this SpriteRenderer self )
+        {
+            return self.transform.position.y + self.bounds.size.y / 2;
+        }
     }
 }
