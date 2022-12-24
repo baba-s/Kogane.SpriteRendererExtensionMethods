4af20067e1b2328caa2d2b15b301b79e44b9c8fe
baba-s <shotababa1206@gmail.com>
2022-12-24 15:13:19 +0900

    fix

--- Runtime/SpriteRendererExtensionMethods.cs
+++ Runtime/SpriteRendererExtensionMethods.cs
@@ -11,48 +11,16 @@ namespace Kogane
             self.color = color;
         }
 
-        public static Vector2 GetSize( this SpriteRenderer self )
-        {
-            return new( self.GetWidth(), self.GetHeight() );
-        }
+        public static Vector2 GetSize( this       SpriteRenderer self ) => new( self.GetWidth(), self.GetHeight() );
+        public static float   GetWidth( this      SpriteRenderer self ) => self.bounds.size.x;
+        public static float   GetWidthHalf( this  SpriteRenderer self ) => self.GetWidth() * 0.5f;
+        public static float   GetHeight( this     SpriteRenderer self ) => self.bounds.size.y;
+        public static float   GetHeightHalf( this SpriteRenderer self ) => self.GetHeight() * 0.5f;
 
-        public static float GetWidth( this SpriteRenderer self )
-        {
-            return self.bounds.size.x;
-        }
-
-        public static float GetHeight( this SpriteRenderer self )
-        {
-            return self.bounds.size.y;
-        }
-
-        public static Rect GetWorldEdge( this SpriteRenderer self )
-        {
-            return new
-            (
-                position: self.transform.position,
-                size: self.bounds.size
-            );
-        }
-
-        public static float GetWorldLeft( this SpriteRenderer self )
-        {
-            return self.transform.position.x - self.bounds.size.x / 2;
-        }
-
-        public static float GetWorldRight( this SpriteRenderer self )
-        {
-            return self.transform.position.x + self.bounds.size.x / 2;
-        }
-
-        public static float GetWorldBottom( this SpriteRenderer self )
-        {
-            return self.transform.position.y - self.bounds.size.y / 2;
-        }
-
-        public static float GetWorldTop( this SpriteRenderer self )
-        {
-            return self.transform.position.y + self.bounds.size.y / 2;
-        }
+        public static Rect  GetWorldEdge( this   SpriteRenderer self ) => new( self.transform.position, self.bounds.size );
+        public static float GetWorldLeft( this   SpriteRenderer self ) => self.transform.position.x - self.bounds.size.x / 2;
+        public static float GetWorldRight( this  SpriteRenderer self ) => self.transform.position.x + self.bounds.size.x / 2;
+        public static float GetWorldBottom( this SpriteRenderer self ) => self.transform.position.y - self.bounds.size.y / 2;
+        public static float GetWorldTop( this    SpriteRenderer self ) => self.transform.position.y + self.bounds.size.y / 2;
     }
 }
