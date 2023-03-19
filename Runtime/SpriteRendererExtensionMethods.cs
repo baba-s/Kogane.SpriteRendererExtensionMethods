7c1f4f9b2744ecfd528c2a9f0c9a0e01f22987b3
baba-s <shotababa1206@gmail.com>
2023-03-19 20:34:42 +0900

    add

--- Runtime/SpriteRendererExtensionMethods.cs
+++ Runtime/SpriteRendererExtensionMethods.cs
@@ -11,6 +11,20 @@ namespace Kogane
             self.color = color;
         }
 
+        public static void SetSizeX( this SpriteRenderer self, float x )
+        {
+            var size = self.size;
+            size.x    = x;
+            self.size = size;
+        }
+
+        public static void SetSizeY( this SpriteRenderer self, float y )
+        {
+            var size = self.size;
+            size.y    = y;
+            self.size = size;
+        }
+
         public static Vector2 GetSize( this       SpriteRenderer self ) => new( self.GetWidth(), self.GetHeight() );
         public static float   GetWidth( this      SpriteRenderer self ) => self.bounds.size.x;
         public static float   GetWidthHalf( this  SpriteRenderer self ) => self.GetWidth() * 0.5f;
@@ -22,5 +36,17 @@ namespace Kogane
         public static float GetWorldRight( this  SpriteRenderer self ) => self.transform.position.x + self.bounds.size.x / 2;
         public static float GetWorldBottom( this SpriteRenderer self ) => self.transform.position.y - self.bounds.size.y / 2;
         public static float GetWorldTop( this    SpriteRenderer self ) => self.transform.position.y + self.bounds.size.y / 2;
+
+        public static Rect  GetLocalEdge( this   SpriteRenderer self ) => new( self.transform.localPosition, self.bounds.size );
+        public static float GetLocalLeft( this   SpriteRenderer self ) => self.transform.localPosition.x - self.bounds.size.x / 2;
+        public static float GetLocalRight( this  SpriteRenderer self ) => self.transform.localPosition.x + self.bounds.size.x / 2;
+        public static float GetLocalBottom( this SpriteRenderer self ) => self.transform.localPosition.y - self.bounds.size.y / 2;
+        public static float GetLocalTop( this    SpriteRenderer self ) => self.transform.localPosition.y + self.bounds.size.y / 2;
+
+        public static void SetColorIfNotNull( this SpriteRenderer self, in Color color )
+        {
+            if ( self == null ) return;
+            self.color = color;
+        }
     }
 }
